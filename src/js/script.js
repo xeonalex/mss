ee40d59b2704f23997a4570c9d9bb183f066011f
5tka <ferol2010@yandex.ru>
2016-06-03 22:01:50 +0300

    footer, gallery-partners

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
-console.log('hello from script.js');
+$(document).ready(function(){
+  // Сладер на главной странице
+  $(".gallery-banners").owlCarousel({
+  	items: 1,
+  	nav: true,
+  	navText: [],
+    loop: true,
+  	// autoplay:true,
+    navigation: true,
+    pagination: true,
+  	responsive: {
+  	}
+  });
+
+});
