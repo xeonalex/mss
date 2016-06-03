2a37b2e844fc2a789514f87f1750451f32696bf8
Sasha Saulyak <lux1995@yandex.ru>
2016-06-03 22:13:32 +0300

    added header / slider at main page

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,20 @@
-console.log('hello from script.js');
+$(document).ready(function(){
+	$('.init_block').on('click', function(event) {
+		event.preventDefault();
+		$(this).toggleClass('init_activated');
+
+	});
+
+	$('.carousel-main').owlCarousel({
+  	items: 1,
+  	nav: true,
+  	pagination: false,
+  	navText: [],
+    loop: true,
+  	autoplay:true,
+  	responsive: {
+  	}
+  });
+	$(".carousel-wrap").css({'visibility': 'visible', 'opacity':'1' });
+});
+
